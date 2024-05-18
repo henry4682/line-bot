c7045a1d0cf8760624b65a3e6d433f6e7498c2c7
henry4682 <henry194557@gmail.com>
2024-05-18 12:55:32 +0800

    new test

---
+++ line-bot.py
@@ -0,0 +1,34 @@
+from flask import Flask
+app = Flask(__name__)
+
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# print(os.environ.get('line_api'))
+line_bot_api = LineBotApi(os.environ.get('line_api'))
+handler = WebhookHandler(os.environ.get('handler'))
+
+@app.route('/callback', methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handler_message(event):
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
+
+if __name__ == '__main__':
+    app.run()
+
